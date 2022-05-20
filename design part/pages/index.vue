3f4a2b32103cf43f55026219570192c9a76cd3c8
RezaPaki <paki.reza@yahoo.com>
2022-05-20 11:39:24 +0200

    project created

---
+++ design part/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <custom-page :title="title" :image="image" :description="description" />
+</template>
+
+<script>
+import CustomPage from '~/components/CustomPage.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    CustomPage,
+  },
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('/api/page-info/index')
+    const title = data.title
+    const image = data.image
+    const description = data.description
+    return {
+      title,
+      description,
+      image,
+    }
+  },
+}
+</script>
