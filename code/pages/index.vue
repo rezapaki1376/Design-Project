30de949d7bc9c9150450919388e86740419cc15d
RezaPaki <paki.reza@yahoo.com>
2022-06-28 20:00:08 +0200

    login page

--- code/pages/index.vue
+++ code/pages/index.vue
@@ -1,71 +1,34 @@
 <template>
-  <div>
+<div>
+
+  <div class="main_index">
     <div class="container">
-      <div id="demo" class="carousel slide" data-bs-ride="carousel">
-        <!-- Indicators/dots -->
-        <div class="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#demo"
-            data-bs-slide-to="0"
-            class="active"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#demo"
-            data-bs-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#demo"
-            data-bs-slide-to="2"
-          ></button>
-        </div>
-
-        <!-- The slideshow/carousel -->
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="index1.jpg" alt="Los Angeles" class="d-block w-60" />
-            <div class="carousel-caption">
-              <h3>New York</h3>
-              <p>We love the Big Apple!</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="index2.jpg" alt="Chicago" class="d-block w-60" />
-            <div class="carousel-caption">
-              <h3>New York</h3>
-              <p>We love the Big Apple!</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="index3.jpg" alt="New York" class="d-block w-60" />
-            <div class="carousel-caption">
-              <h3>New York</h3>
-              <p>We love the Big Apple!</p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Left and right controls/icons -->
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#demo"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon"></span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#demo"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon"></span>
-        </button>
+      <div class="row">
+      <div class="col-lg-6">
+        <h1>Let's see Rome with us</h1>
+      </div>
+      <div class="col-lg-6">
+        <p>As we planned our first international trip with baby J we sought inspiration and motivation from our favourite family travel quotes: from bravery inducing adventure travel quotes to wonderful wanderlust quotes, we hope that our collection of the best family travel quotes will inspire you on your travels and encourage your wandering soul!</p>
       </div>
     </div>
+    <div class="row image-box">
+      <div class="col-lg-4">
+        <img src="index1.jpg" alt="">
+      </div>
+      <div class="col-lg-4">
+        <img src="index2.jpg" alt="">
+      </div>
+      <div class="col-lg-4">
+        <img src="index3.jpg" alt="">
+      </div>
+    </div>
+
+
+
+      </div>
+      
+    
+  </div>
   </div>
 </template>
 
@@ -92,23 +55,38 @@ export default {
       events: [],
     }
   },
-  async mounted() {},
+  // async mounted() {},
 }
 </script>
 <style scoped>
-.row * {
-  padding: 0 !important;
-  margin: 0;
+
+.main_index{
+  background: #292d33;
+  color: white;
+  padding: 5% 0;
 }
-.row {
-  padding: 0;
-  margin: 0;
-}
-.carousel-inner img {
+img{
   width: 100%;
-  height: 600px;
+  height: 100%;
+  padding:0 2.5px;
 }
-.carousel-indicators button {
-  margin: 0 10px;
+img:hover{
+  cursor: pointer;
+  filter: blur(0.4px);
+  
 }
+h1{
+  font-size: 70px;
+}
+p{
+  font-size: 20px;
+}
+@media (min-width: 1200px) {
+  .container{
+  max-width: 70%;
+}
+  
+}
+
+
 </style>
