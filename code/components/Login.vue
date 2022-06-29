92cc34349f8c9e4a651e8e9e4d69e3a759ef4a4f
RezaPaki <sajadhamzenejadi76@gmail.com>
2022-06-29 10:16:46 +0200

    service page

--- code/components/Login.vue
+++ code/components/Login.vue
@@ -22,15 +22,15 @@
 		  <div class="col-lg-6 col-md-6 col-sm-6"><form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input id="exampleInputEmail1" type="email" class="form-control"  aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input id="exampleInputPassword1" type="password" class="form-control"  placeholder="Password">
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <input id="exampleCheck1" type="checkbox" class="form-check-input" >
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
   <button type="submit" class="btn btn-success">Login</button>
