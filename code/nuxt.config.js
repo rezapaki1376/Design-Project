afb6af2bc827f53d529b55cf74e6b6ae610467e8
aminhofmann <aminhofmann1994@gmail.com>
2022-07-05 11:44:47 +0200

    fix: config chagne

--- code/nuxt.config.js
+++ code/nuxt.config.js
@@ -70,8 +70,10 @@ const config = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000/api',
-    // baseURL: 'https://find-your-rome.herokuapp.com/api',
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://find-your-rome.herokuapp.com/api'
+        : 'http://localhost:3000/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
